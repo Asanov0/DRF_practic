3030152a93aa15385b8fdbd7dc3b40469e48457e
Azatbek <mairanbekuuluazatbek@gmail.com>
2023-05-08 02:00:50 +0600

    second added

--- just_practic/urls.py
+++ just_practic/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
-from django.urls import path
-from .views import test
+from django.urls import path, include
+from just_practic.views import *
 
 
 
+app_name = 'just_practic'
 urlpatterns = [
-    path('', test, name='test'),
+    # path('', test, name='test'),
+    path('', CarCreateView.as_view()),
 ]
--- just_practic/views.py
+++ just_practic/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+from rest_framework import generics
+from just_practic.serializers import CarDetailSerializer
 
 
 def test(request):
     return HttpResponse('hello every one is this work ?')
+
+
+
+class CarCreateView(generics.CreateAPIView):
+    serializer_class = CarDetailSerializer
+   
